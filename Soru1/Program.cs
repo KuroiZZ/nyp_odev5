c439ceffb77f21f2fc780fb5c99871a98ed0d005
Habil Tatarogullari <habiltatar01@gmail.com>
2023-12-15 14:48:43 +0300

    Hata vermesi gerekebilen bir özellik belirtildi. Cözüm araniyor

--- Soru1/Program.cs
+++ Soru1/Program.cs
@@ -5,12 +5,8 @@ namespace Workspace
 {
     public class Hemisphere
     {
+        //Radius'a main icinde radius'a 0 ile 12 arası bir değer girilmezse otomatik 1 oluyor //Hata mı Vermeli yoksa bunu mu yapmalı ??
         private double radius = 1;
-
-        private double volume;
-        private double curvedSurfaceArea;
-        private double totalSurfaceArea;
-        
         public double Radius
         {
             get{return radius;}
@@ -23,7 +19,8 @@ namespace Workspace
                 }
             }
         }
-        
+
+        private double volume;
         public double Volume
         {
             get
@@ -32,7 +29,9 @@ namespace Workspace
                 return volume;
             }
         }
+        
 
+        private double curvedSurfaceArea;
         public double CurvedSurfaceArea
         {
             get
@@ -41,7 +40,8 @@ namespace Workspace
                 return curvedSurfaceArea;
             }
         }
-    
+
+        private double totalSurfaceArea;
         public double TotalSurfaceArea
         {
             get
@@ -63,9 +63,13 @@ namespace Workspace
             Hemisphere hemisphere2 = new Hemisphere();
             hemisphere2.Radius = 8;
 
-            Console.WriteLine("Volume1:            {0}  ||  Volume2:            {1}",hemisphere1.Volume,hemisphere2.Volume);
+            //hemisphere1.Radius = 13;
+
+            Console.WriteLine("Volume1:            {0}  ||  Volume2:            {1}",hemisphere1.volume,hemisphere2.volume);
             Console.WriteLine("CurvedSurfaceArea1: {0}   ||  CurvedSurfaceArea2: {1}",hemisphere1.CurvedSurfaceArea,hemisphere2.CurvedSurfaceArea);
             Console.WriteLine("TotalSurfaceArea1:  {0}    ||  TotalSurfaceArea2:  {1}",hemisphere1.TotalSurfaceArea,hemisphere2.TotalSurfaceArea);
+
+            //hemisphere1.Volume = 12;  
         }
     }
 }
