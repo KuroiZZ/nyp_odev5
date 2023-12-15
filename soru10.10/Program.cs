d029cc9113f1854a4c27f430748e2a674161ef7d
Güneş Balcı <gunesbalci66@hotmail.com>
2023-12-15 15:40:32 +0300

    10.10 Tamamlandı. %100

--- soru10.10/Program.cs
+++ soru10.10/Program.cs
@@ -18,7 +18,7 @@ namespace Odev
             playGround = new int[3,3] {{0,0,0},{0,0,0},{0,0,0}};
         }
 
-        internal bool Move(int[] square,ref bool player1)
+        internal bool Move(int[] square,ref bool player1,ref int turn)
         {
             bool illegalMove = false;
             if(playGround[square[0]-1,square[1]-1]==0)
@@ -35,24 +35,26 @@ namespace Odev
             else
             {
                 illegalMove = true;
-                Console.Write("Illegal move\n");
+                Console.Write("\nERROR: Illegal move\nTry Again!\n\n");
             }
             if(!illegalMove)
+            {
+                turn +=1;
                 player1 = !player1;
-            /*if(player1)
-            {
-                player1 = false;
             }
-            else
-            {
-                player1 = true;
-            }*/
-
             return illegalMove;
         }
 
-        internal int[] askSquare()
+        internal int[] askSquare(int turn,bool player1)
         {
+            if(player1)
+            {
+                Console.Write($"Turn {turn}: Player 1's turn");
+            }
+            else
+            {
+                Console.Write($"Turn {turn}: Player 2's turn");
+            }
             int[] square;
             square = new int[] {0,0};
             Console.Write("\nRow: ");
@@ -64,57 +66,76 @@ namespace Odev
 
         internal void letPlay()
         {
+            int turn = 0;
             bool win = false;
             bool illegalMove = false;
             bool player1= true;
+            bool Player1win = false;
             int[] square = new int[2];
-            while(!win)
+            playGroundPrint();
+            while(!win && turn < 9)
             {
                 if(!illegalMove)
                 {
-                    square = askSquare();
-                    illegalMove = Move(square,ref player1);
-                    win = controlWin();
-                    playGroundPrint();
+                    square = askSquare(turn+1,player1);
+                    illegalMove = Move(square,ref player1,ref turn);
+                    win = controlWin(ref Player1win);
+                    if(!illegalMove)
+                    {
+                        playGroundPrint();
+                    }
                 }
                 while(illegalMove)
                 {
-                    square = askSquare();
-                    illegalMove = Move(square,ref player1);
-                    win = controlWin();
+                    square = askSquare(turn+1,player1);
+                    illegalMove = Move(square,ref player1,ref turn);
+                    win = controlWin(ref Player1win);
                     playGroundPrint();
                 }
             }
+            if(win && Player1win)
+            {
+                Console.Write("Player 1 Won!\n");
+            }
+            else if(win && !Player1win)
+            {
+                Console.Write("Player 2 Won!\n");
+            }
+            else if(!win && turn==9)
+            {
+                Console.Write("Its draw!\n");
+            }
         }
         
-        internal bool controlWin()
+        internal bool controlWin(ref bool Player1win)
         {
             bool win = false;
+            Player1win = false;
             for(int i=0;i<3;i++)
             {
-                if(playGround[i,0]==playGround[i,1]&&playGround[i,1]==playGround[i,2]&&playGround[i,0]!=0)
+                if(playGround[i,0]==playGround[i,1]&&playGround[i,1]==playGround[i,2]&&playGround[i,0]==1)
                 {
+                    Player1win = true;
                     win = true;
-                    Console.Write("Kazandin\n");
                     break;
                 }
-                else if(playGround[0,i]==playGround[1,i]&&playGround[2,i]==playGround[1,i]&&playGround[1,i]!=0)
+                else if(playGround[0,i]==playGround[1,i]&&playGround[2,i]==playGround[1,i]&&playGround[1,i]==1)
                 {
+                    Player1win = true;
                     win = true;
-                    Console.Write("Kazandin\n");
                     break;
                 }
             }
-            if(playGround[0,0]==playGround[1,1]&&playGround[1,1]==playGround[2,2]&&playGround[1,1]!=0)
+            if(playGround[0,0]==playGround[1,1]&&playGround[1,1]==playGround[2,2]&&playGround[1,1]==1)
             {
+                Player1win = true;
                 win = true;
-                Console.Write("Kazandin\n");
                 return win;
             }
-            else if(playGround[0,2]==playGround[1,1]&&playGround[1,1]==playGround[2,0]&&playGround[1,1]!=0)
+            else if(playGround[0,2]==playGround[1,1]&&playGround[1,1]==playGround[2,0]&&playGround[1,1]==1)
             {
+                Player1win = true;
                 win = true;
-                Console.Write("Kazandin\n");
                 return win;
             }
             return win;
@@ -122,6 +143,7 @@ namespace Odev
 
         internal void playGroundPrint()
         {
+            Console.Write("\n");
             for(int i=0;i<3;i++)
             {
                 for(int j=0;j<3;j++)
@@ -130,6 +152,7 @@ namespace Odev
                 }
                 Console.Write("\n");
             }
+            Console.Write("\n");
         }
     }
 
